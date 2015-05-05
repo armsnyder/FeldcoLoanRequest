4fee7d2a1d84b05e765a7ffe1a1516b3efbe136e
friendly-flame <armsnyder@gmail.com>
2015-05-04 21:10:54 -0500

    Placeholder API service, closes #1

--- www/applicationStatus/applicationStatusController.js
+++ www/applicationStatus/applicationStatusController.js
@@ -1,5 +1,10 @@
-angular.module('FeldcoLoanRequest').controller('ApplicationStatusController', function($scope, supersonic) {
-
-
-
+angular.module('FeldcoLoanRequest').controller('ApplicationStatusController', function($scope, supersonic, BankRequestService) {
+    $scope.submitRequest = function() {
+        requestObject = {};
+        BankRequestService.submitRequest(requestObject).then(function(result) {
+            var approved = result.approved;
+        }, function(reason) {
+            supersonic.logger.log('Request Error: '+reason);
+        });
+    };
 });
--- www/applicationStatus/bankRequestService.js
+++ www/applicationStatus/bankRequestService.js
@@ -1,5 +1,36 @@
-angular.module('FeldcoLoanRequest').factory('BankRequestService', function(supersonic) {
-
-
-
+angular.module('FeldcoLoanRequest').factory('BankRequestService', function($q, supersonic) {
+    var factory = {};
+    var banks = {
+        // Placeholder bank statistics used in placeholder algorithm
+        internal: {
+            minCreditScore: 700
+        },
+        wellsFargo: {
+            minCreditScore: 650
+        },
+        greenSky: {
+            minCreditScore: 600
+        }
+    };
+    factory.submitRequest = function(requestObject) {
+        // Placeholder function that takes an object containing form submission data and returns a promise.
+        // requestObject must be a javascript object containing fields for 'bank' and 'creditScore'
+        // A resolved promise comes with a javascript object containing a boolean called 'approved'
+        return $q(function(resolve, reject) {
+            var correctFormat = typeof requestObject == 'object' && 'bank' in requestObject
+                && 'creditScore' in requestObject && requestObject.bank in banks;
+            setTimeout(function() {
+                if (corectFormat) {
+                    if (requestObject.creditScore > banks[requestObject.bank].minCreditScore) {
+                        resolve({approved: true});
+                    } else {
+                        resolve({approved: false});
+                    }
+                } else {
+                    reject('Incorrect request format');
+                }
+            }, 3000);
+        });
+    };
+    return factory;
 });
