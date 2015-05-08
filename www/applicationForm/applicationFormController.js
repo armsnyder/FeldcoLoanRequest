e114a9816453de53849e16e687e9f68d15b0a8fd
Frank Avino <francisavino2015@u.northwestern.edu>
2015-05-08 13:25:03 -0500

    most recent test of the form, angular still not working on applicationForm.html

--- www/applicationForm/applicationFormController.js
+++ www/applicationForm/applicationFormController.js
@@ -1,5 +1,15 @@
-angular.module('FeldcoLoanRequest').controller('ApplicationFormController', function($scope, supersonic) {
+angular
+	.module('FeldcoLoanRequest')
+	.controller('ApplicationFormController', function ($scope, supersonic) {
+	    /*$scope.master = {firstName: "John", lastName: "Doe"};
+    $scope.reset = function() {
+        //$scope.user = angular.copy($scope.master);
+        supersonic.logger.log($scope.user);
+    };
+    $scope.reset();
 
-
-
+	supersonic.logger.log("test0");*/
+	$scope.totalAmount = 5000;
+	$scope.$apply();
+	supersonic.logger.log("test2");
 });
