4a4688efc32ab60651fcbe3a184c109a71ce0e4f
friendly-flame <armsnyder@gmail.com>
2015-05-11 05:48:35 -0500

    demo-able payoff, using hard-coded values

--- app/common/scripts/applicationFormController.js
+++ app/common/scripts/applicationFormController.js
@@ -1,9 +1,10 @@
 angular
 	.module('common')
-	.controller('ApplicationFormController', function ($scope, supersonic, FormService) {
+	.controller('ApplicationFormController', function ($scope, supersonic) {
     $scope.formInfo = {};
     $scope.submit = function(form){
-        FormService.forms.creditForm = angular.copy(form);
+        //FormService.forms.creditForm = angular.copy(form);
+        //supersonic.data.channel('form').publish(form);
         var view = new supersonic.ui.View("common#applicationStatus");
         supersonic.ui.layers.push(view);
     };
