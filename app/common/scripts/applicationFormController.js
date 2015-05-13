c14c2a68d9b2f3434a11f10ef5bc0c39932d9394
Joshua Roberts <joshuapr1@gmail.com>
2015-05-13 17:13:52 -0500

    Added the coapplicants data to the applicants data objects

--- app/common/scripts/applicationFormController.js
+++ app/common/scripts/applicationFormController.js
@@ -1,17 +1,13 @@
 angular
 	.module('common')
 	.controller('ApplicationFormController', function ($scope, supersonic) {
-    $scope.formInfo = {};
-    $scope.showCoapp = false;
+    $scope.creditformInfo = {};
+    $scope.showCoap = false;
     $scope.submit = function(form){
         //FormService.forms.creditForm = angular.copy(form);
-        //if (form2) {
-        //  FormService.forms.coApplicant = angular.copy(form2)
-        //}
         //supersonic.data.channel('form').publish(form);
         var view = new supersonic.ui.View("common#applicationStatus");
         supersonic.ui.layers.push(view);
-        alert("It works");
     };
    $scope.coApplicant = function(bool){
       $scope.showCoapp = bool;
