b0d9925883b1a9f1811411ad9ca5d14dc34e6fe8
Frank Avino <francisavino2015@u.northwestern.edu>
2015-05-14 18:39:58 -0500

    Show only the accepted loan and relevant TIL calculation/Promo templates button

--- app/common/scripts/applicationStatusController.js
+++ app/common/scripts/applicationStatusController.js
@@ -3,35 +3,52 @@ angular.module('common').controller('ApplicationStatusController', function($sco
         feldcoFinance: {
             waiting: true,
             text: 'Declined',
-            amount: 0
+            amount: 0,
+            showFeldco: false
         },
         wellsFargo: {
             waiting: true,
             text: 'Declined',
-            amount: 0
+            amount: 0,
+            showWellsFargo: false
         },
         greenSky: {
             waiting: true,
             text: 'Declined',
-            amount: 0
+            amount: 0,
+            showGreenSky: false
         }
     };
-    //$scope.creditScore = 800*FormService.forms.creditForm.income/100000;
 
+    $scope.noApprovals = false;
+    //$scope.creditScore = 800*FormService.forms.creditForm.income/100000;
+    $scope.waitingForApprovals = true;
     setTimeout(function() {
         $scope.approvals.feldcoFinance.waiting = false;
-        $scope.approvals.feldcoFinance.text = 'Declined';
-        $scope.approvals.feldcoFinance.amount = '';
+        $scope.approvals.feldcoFinance.text = 'Approved';
+        $scope.approvals.feldcoFinance.amount = '$60,000';
+        $scope.approvals.feldcoFinance.showFeldco = true;
+        if($scope.approvals.feldcoFinance.showFeldco){
+            $scope.waitingForApprovals = false;
+        }
         supersonic.logger.log(1);
         setTimeout(function() {
             $scope.approvals.wellsFargo.waiting = false;
-            $scope.approvals.wellsFargo.text = 'Declined';
-            $scope.approvals.wellsFargo.amount = '';
+            $scope.approvals.wellsFargo.text = 'Approved';
+            $scope.approvals.wellsFargo.amount = '$36,000';
+            $scope.approvals.wellsFargo.showWellsFargo = false;
+            if($scope.approvals.wellsFargo.showWellsFargo){
+                $scope.waitingForApprovals = false;
+            }
             supersonic.logger.log(2);
             setTimeout(function() {
                 $scope.approvals.greenSky.waiting = false;
                 $scope.approvals.greenSky.text = 'Approved';
                 $scope.approvals.greenSky.amount = '$12,000';
+                $scope.approvals.greenSky.showGreenSky = false;
+                if($scope.approvals.greenSky.showGreenSky){
+                    $scope.waitingForApprovals = false;
+                }
                 supersonic.logger.log(3);
             }, 2000)
         }, 2000)
@@ -44,6 +61,7 @@ angular.module('common').controller('ApplicationStatusController', function($sco
             if (approved) {
                 $scope.approvals.feldcoFinance.text = 'Approved';
                 $scope.approvals.feldcoFinance.amount = FormService.forms.creditForm.loanAmount;
+                        $scope.approvals.feldcoFinance.showFeldco = true;
             } else {
                 $scope.approvals.feldcoFinance.text = 'Declined';
                 requestObject = {bank: 'wellsFargo', creditScore: $scope.creditScore};
@@ -52,6 +70,7 @@ angular.module('common').controller('ApplicationStatusController', function($sco
                     if (approved) {
                         $scope.approvals.wellsFargo.text = 'Approved';
                         $scope.approvals.wellsFargo.amount = FormService.forms.creditForm.loanAmount;
+                        $scope.approvals.wellsFargo.showWellsFargo = true;
                     } else {
                         $scope.approvals.wellsFargo.text = 'Declined';
                         requestObject = {bank: 'greenSky', creditScore: $scope.creditScore};
@@ -60,9 +79,10 @@ angular.module('common').controller('ApplicationStatusController', function($sco
                             if (approved) {
                                 $scope.approvals.greenSky.text = 'Approved';
                                 $scope.approvals.greenSky.amount = FormService.forms.creditForm.loanAmount;
+                                $scope.approvals.greenSky.showGreenSky = true;
                             } else {
                                 $scope.approvals.greenSky.text = 'Declined';
-
+                                $scope.noApprovals = true;
                             }
                         }, function(reason) {
                             supersonic.logger.log('Request Error: '+reason);
