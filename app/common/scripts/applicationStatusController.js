d4c02d36cb33b326787488752d7e03e46f31076f
Frank Avino <francisavino2015@u.northwestern.edu>
2015-05-14 19:12:23 -0500

    While request is waiting for approval, user can see the name of the bank that is being contacted

--- app/common/scripts/applicationStatusController.js
+++ app/common/scripts/applicationStatusController.js
@@ -23,16 +23,26 @@ angular.module('common').controller('ApplicationStatusController', function($sco
     $scope.noApprovals = false;
     //$scope.creditScore = 800*FormService.forms.creditForm.income/100000;
     $scope.waitingForApprovals = true;
+
+    $scope.contactingFeldco = true;
+    $scope.contactingWellsFargo = false;
+    $scope.contactingGreenSky = false;
+
     setTimeout(function() {
+        $scope.contactingFeldco = false;
         $scope.approvals.feldcoFinance.waiting = false;
         $scope.approvals.feldcoFinance.text = 'Approved';
         $scope.approvals.feldcoFinance.amount = '$60,000';
-        $scope.approvals.feldcoFinance.showFeldco = true;
+        $scope.approvals.feldcoFinance.showFeldco = false;
         if($scope.approvals.feldcoFinance.showFeldco){
             $scope.waitingForApprovals = false;
         }
+        else{
+            $scope.contactingWellsFargo = true;
+        }
         supersonic.logger.log(1);
         setTimeout(function() {
+            $scope.contactingWellsFargo = false;
             $scope.approvals.wellsFargo.waiting = false;
             $scope.approvals.wellsFargo.text = 'Approved';
             $scope.approvals.wellsFargo.amount = '$36,000';
@@ -40,8 +50,12 @@ angular.module('common').controller('ApplicationStatusController', function($sco
             if($scope.approvals.wellsFargo.showWellsFargo){
                 $scope.waitingForApprovals = false;
             }
+            else{
+                $scope.contactingGreenSky = true;
+            }
             supersonic.logger.log(2);
             setTimeout(function() {
+                $scope.contactingGreenSky = false;
                 $scope.approvals.greenSky.waiting = false;
                 $scope.approvals.greenSky.text = 'Approved';
                 $scope.approvals.greenSky.amount = '$12,000';
@@ -49,6 +63,8 @@ angular.module('common').controller('ApplicationStatusController', function($sco
                 if($scope.approvals.greenSky.showGreenSky){
                     $scope.waitingForApprovals = false;
                 }
+                $scope.waitingForApprovals = false;
+                $scope.noApprovals = true;
                 supersonic.logger.log(3);
             }, 2000)
         }, 2000)
@@ -62,6 +78,7 @@ angular.module('common').controller('ApplicationStatusController', function($sco
                 $scope.approvals.feldcoFinance.text = 'Approved';
                 $scope.approvals.feldcoFinance.amount = FormService.forms.creditForm.loanAmount;
                         $scope.approvals.feldcoFinance.showFeldco = true;
+                        $scope.waitingForApprovals = false;
             } else {
                 $scope.approvals.feldcoFinance.text = 'Declined';
                 requestObject = {bank: 'wellsFargo', creditScore: $scope.creditScore};
@@ -71,6 +88,7 @@ angular.module('common').controller('ApplicationStatusController', function($sco
                         $scope.approvals.wellsFargo.text = 'Approved';
                         $scope.approvals.wellsFargo.amount = FormService.forms.creditForm.loanAmount;
                         $scope.approvals.wellsFargo.showWellsFargo = true;
+                        $scope.waitingForApprovals = false;
                     } else {
                         $scope.approvals.wellsFargo.text = 'Declined';
                         requestObject = {bank: 'greenSky', creditScore: $scope.creditScore};
@@ -80,8 +98,10 @@ angular.module('common').controller('ApplicationStatusController', function($sco
                                 $scope.approvals.greenSky.text = 'Approved';
                                 $scope.approvals.greenSky.amount = FormService.forms.creditForm.loanAmount;
                                 $scope.approvals.greenSky.showGreenSky = true;
+                                $scope.waitingForApprovals = false;
                             } else {
                                 $scope.approvals.greenSky.text = 'Declined';
+                                $scope.waitingForApprovals = false;
                                 $scope.noApprovals = true;
                             }
                         }, function(reason) {
