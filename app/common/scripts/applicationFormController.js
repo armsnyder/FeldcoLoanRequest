8ff3e2d7dbeafeab7417220328122d29e2cb024f
Frank Avino <francisavino2015@u.northwestern.edu>
2015-05-17 22:33:56 -0500

    First draft of logic for simulating bank decisions based on application form input

--- app/common/scripts/applicationFormController.js
+++ app/common/scripts/applicationFormController.js
@@ -2,12 +2,20 @@ angular
 	.module('common')
 	.controller('ApplicationFormController', function ($scope, supersonic) {
     $scope.creditformInfo = {};
-    $scope.showCoap = false;
+    $scope.showCoapp = false;
+    
     $scope.submit = function(form){
         //FormService.forms.creditForm = angular.copy(form);
         //supersonic.data.channel('form').publish(form);
         var view = new supersonic.ui.View("common#applicationStatus");
         supersonic.ui.layers.push(view);
+        localStorage.totalAmount = JSON.stringify($scope.creditformInfo.totalAmount);
+        localStorage.loanAmount = JSON.stringify($scope.creditformInfo.loanAmount);
+        localStorage.yearInHouse = JSON.stringify($scope.creditformInfo.yearInHouse);
+        localStorage.ownProperty = JSON.stringify($scope.creditformInfo.property);
+        localStorage.yearsOnJob = JSON.stringify($scope.creditformInfo.yearsOnJob);
+        localStorage.annualIncome = JSON.stringify($scope.creditformInfo.income);
+        localStorage.coAppIncome = JSON.stringify($scope.creditformInfo.coAppIncome);
     };
    $scope.coApplicant = function(bool){
       $scope.showCoapp = bool;
