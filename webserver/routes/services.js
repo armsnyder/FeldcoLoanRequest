e169c2f7813f43b3f18509ac443430335d6dafa8
Frank Avino <francisavino2015@u.northwestern.edu>
2015-05-19 20:15:57 -0500

    added fake usernames and login request logic to services.js

--- webserver/routes/services.js
+++ webserver/routes/services.js
@@ -1,12 +1,37 @@
 // EXAMPLES:
 
+var fakeLogin = {
+    user1: {
+        username: 'Tim Smith',
+        password: 'password'
+    },
+    user2: {
+        username: 'John Smith',
+        password: 'password'
+    },
+    user3: {
+        username: 'Tom Smith',
+        password: 'password'
+    }
+};
+
 // more info at http://expressjs.com/4x/api.html#router
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('index', {title: 'Express'});
         });
-    app.post('/button', function(req, res) {
+    app.post('/dummyAPI/login', function(req, res) {
+
+        //decide here if the req.username matches one of the usernames,
+        //and if the req.password matches 'password'
+
+        if((req.data.username == fakeLogin.user1.username) || (req.data.username == fakeLogin.user2.username) || (req.data.username == fakeLogin.user3.username)){
+            if(req.data.password == 'password'){
+                //return true?
+            }
+        }
+
         data = req.body;
         res.send();
     });
