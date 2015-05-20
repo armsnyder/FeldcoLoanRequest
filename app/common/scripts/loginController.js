0b12980ee00b5c161c417dd348fbda642801efe3
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-05-19 20:01:12 -0500

    added angular form logic for login form

--- app/common/scripts/loginController.js
+++ app/common/scripts/loginController.js
@@ -1,5 +1,19 @@
-angular.module('common').controller('LoginController', function($scope, supersonic) {
+angular
+	.module('common')
+	.controller('LoginController', function ($scope, supersonic) {
 
+		$scope.login = function(user) {
+			$scope.master = angular.copy(user);
+			
+			$http.post('/login', {username: $scope.master.username,
+			                      password: $scope.master.password}).
+				success(function(data) {
+					supersonic.ui.initialView.dismiss();
+				}).
+				error(function(data) {
+					
+				});
+		};
 
 
 });
