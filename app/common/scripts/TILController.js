8b0a73851b05ae1f40a937e745cae0ca76d82001
YuxuanHe <yuxuanhe2016@u.northwestern.edu>
2015-05-28 15:04:39 -0500

    ADD CONTROL

--- app/common/scripts/TILController.js
+++ app/common/scripts/TILController.js
@@ -1,6 +1,9 @@
 angular.module('common').controller('TILController', function($scope, supersonic) {
   $scope.Calculate = function(){
     $scope.box5 = $scope.TILInfo.depositAmount;
+    $scope.BOX_3 = $scope.TILInfo.financeAmount;
+    $scope.BOX_4 = $scope.TILInfo.financeAmount;// should be add box 2 also
+    $scope.BOX_1 = $scope.TILInfo.APRAmount;
     $scope.apply();
   }
 });
