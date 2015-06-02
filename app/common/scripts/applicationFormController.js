523ef850cc3fa6af98023284fd25808c4d2ac7c1
Frank Avino <francisavino2015@u.northwestern.edu>
2015-06-01 21:01:35 -0500

    form validation requires dollar amounts for total amount for contracts, loan amount, and annual income

--- app/common/scripts/applicationFormController.js
+++ app/common/scripts/applicationFormController.js
@@ -52,8 +52,11 @@ angular
           } else if($scope.creditformInfo.property == ""){
             supersonic.ui.dialog.alert("Please select a property type.");
         } */
-        if(!($scope.creditformInfo.totalAmount && $scope.creditformInfo.loanAmount && $scope.creditformInfo.plan && $scope.creditformInfo.phoneBest && $scope.creditformInfo.phoneWork && $scope.creditformInfo.firstName && $scope.creditformInfo.lastName && $scope.creditformInfo.dateOfBirth && $scope.creditformInfo.socialsecurityPartOne && $scope.creditformInfo.socialsecurityPartTwo && $scope.creditformInfo.socialsecurityPartThree && $scope.creditformInfo.property)){
+        if(!($scope.creditformInfo.totalAmount && $scope.creditformInfo.loanAmount && $scope.creditformInfo.plan && $scope.creditformInfo.phoneBest && $scope.creditformInfo.firstName && $scope.creditformInfo.lastName && $scope.creditformInfo.dateOfBirth && $scope.creditformInfo.addressStreet && $scope.creditformInfo.addressCity && $scope.creditformInfo.addressZipCode && $scope.creditformInfo.socialsecurityPartOne && $scope.creditformInfo.socialsecurityPartTwo && $scope.creditformInfo.socialsecurityPartThree && $scope.creditformInfo.yearInHouse && $scope.creditformInfo.property && $scope.creditformInfo.email && $scope.creditformInfo.employerName && $scope.creditformInfo.yearsOnJob && $scope.creditformInfo.income)){
           supersonic.ui.dialog.alert("You are missing one or more form fields.");
+        }
+        else if (!($scope.creditformInfo.totalAmount.charAt(0) == "$") || !($scope.creditformInfo.loanAmount.charAt(0) == "$") || !($scope.creditformInfo.income.charAt(0) == "$")){
+          supersonic.ui.dialog.alert("Enter a dollar amount for all fields that require dollars.");
         }
         else{
           view = new supersonic.ui.View("common#applicationStatus");
