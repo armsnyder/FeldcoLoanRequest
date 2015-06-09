74735be2bc240312dee3f479eff032cbc2505c90
missfxhr <xiaofang2016@u.northwestern.edu>
2015-06-09 14:16:45 -0500

    almost done

--- EndToEndTest-XiaoFang/src/com/AppiumTest/AppiumTest.java
+++ EndToEndTest-XiaoFang/src/com/AppiumTest/AppiumTest.java
@@ -30,17 +30,24 @@ public class AppiumTest {
 	@Test
 	public void loginTest() throws InterruptedException{
 		System.out.println("start!");
+		Thread.sleep(15000);
 		//List<WebElement> textField = driver.findElements(By.className("android.widget.EditText"));
-		//driver.findElement(By.className("android.widget.EditText")).sendKeys("Admin");
-		//driver.findElement(By.id("test2")).sendKeys("Admin");
+		driver.findElement(By.xpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.webkit.WebView[1]/android.view.View[1]/android.view.View[1]/android.widget.EditText[1]")).sendKeys("Admin");
+		//Thread.sleep(2000);
+		driver.findElement(By.xpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.webkit.WebView[1]/android.view.View[1]/android.view.View[1]/android.widget.EditText[2]")).sendKeys("Admin");
+		//Thread.sleep(2000);
+		driver.findElement(By.xpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.webkit.WebView[1]/android.view.View[1]/android.view.View[1]/android.widget.Button[1]")).click();
 		//driver.findElement(By.linkText("username")).sendKeys("Admin");
-		Thread.sleep(30000);
+		driver.findElement(By.xpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.webkit.WebView[1]/android.view.View[1]/android.view.View[1]/android.widget.Button[1]")).click();
+		Thread.sleep(20000);
 		
 		//driver.findElements(By.xpath("android.widge.Button")).get(1).click();
 		//driver.findElement(By.className("android.widget.Button")).click();
 		//Thread.sleep(3000);
 		
 		System.out.println("succeeded!");
+		
+		Thread.sleep(45000);
 	}
 	
 	@AfterMethod
