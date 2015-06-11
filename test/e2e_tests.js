1ecf11b80b5f336acadf916f73bab6dbe7e04b1a
friendly-flame <armsnyder@gmail.com>
2015-06-10 23:37:49 -0500

    Final appium testing

--- test/e2e_tests.js
+++ test/e2e_tests.js
@@ -11,14 +11,14 @@ describe("Feldco", function () {
     this.timeout(30000);
     var driver;
     var allPassed = true;
+    var desired;
 
     before(function () {
         var serverConfig = process.env.SAUCE ?
             serverConfigs.sauce : serverConfigs.local;
         driver = wd.promiseChainRemote(serverConfig);
         require("./helpers/logging").configure(driver);
-
-        var desired = _.clone(require("./helpers/caps").mycapabilities);
+        desired = _.clone(require("./helpers/caps").mycapabilities);
         desired.app = require("./helpers/apps").myTestApp;
         if (process.env.SAUCE) {
             desired.name = 'Feldco';
@@ -26,6 +26,10 @@ describe("Feldco", function () {
         }
         return driver.init(desired);
     });
+
+    //beforeEach(function() {
+    //    return driver.init(desired);
+    //});
 
     after(function () {
         return driver
@@ -39,6 +43,7 @@ describe("Feldco", function () {
 
     afterEach(function () {
         allPassed = allPassed && this.currentTest.state === 'passed';
+        return driver.quit().init(desired);
     });
 
     it("should not accept false credentials", function () {
@@ -50,6 +55,29 @@ describe("Feldco", function () {
             .elementByXPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIAButton[1]").click()
             .elementByXPath("//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAStaticText[1]").text()
             .should.eventually.equal('Log In');
+    });
+
+    it("should accept good credentials", function () {
+        return driver
+            .elementByXPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIATextField[1]").sendKeys("John Smith")
+            .elementByXPath("//UIAApplication[1]/UIAWindow[2]/UIAToolbar[1]/UIAButton[2]").click()
+            .elementByXPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIASecureTextField[1]").sendKeys("password")
+            .elementByXPath("//UIAApplication[1]/UIAWindow[2]/UIAToolbar[1]/UIAButton[3]").click()
+            .elementByXPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIAButton[1]").click()
+            .elementByXPath("//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[2]/UIAStaticText[1]")
+            .should.eventually.exist;
+    });
+
+    it("should allow administrators to change bank routing", function () {
+        return driver
+            .elementByXPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIATextField[1]").sendKeys("admin")
+            .elementByXPath("//UIAApplication[1]/UIAWindow[2]/UIAToolbar[1]/UIAButton[2]").click()
+            .elementByXPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIASecureTextField[1]").sendKeys("admin")
+            .elementByXPath("//UIAApplication[1]/UIAWindow[2]/UIAToolbar[1]/UIAButton[3]").click()
+            .elementByXPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIAButton[1]").click()
+            .elementByXPath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIAButton[1]").click();
+            //.press({x: 328, y: 310}).wait(100).moveTo({x: 330, y: 233}).release().perform();
+            //.execute("mobile: swipe", { "touchCount": 1, "startX": 328, "startY": 310, "endX": 330, "endY": 233, "duration": 0.87060546875 })
     });
 
 });
